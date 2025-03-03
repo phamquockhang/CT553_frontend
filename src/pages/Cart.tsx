d821f31d2a15b6a56568f18d6394911c8987490e
Pham Quoc Khang <phamquockhang1108@gmail.com>
2025-03-03 21:53:13 +0700

    refactor: Cart component to use cart data from state

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -16,34 +16,38 @@ import { useDynamicTitle } from "../utils";
 
 const Cart: React.FC = () => {
   useDynamicTitle("Giỏ hàng");
+
+  const { updateCartDetail } = UpdateCartDetail();
+  const { deleteCartDetail } = DeleteCartDetail();
+  const { cartState, cartDispatch } = useCartData();
+  const navigate = useNavigate();
+
   const customer: ICustomer | null = JSON.parse(
     localStorage.getItem("customer") || "null",
   );
-  const cart = customer?.cart;
+
+  const cartData = customer && customer.customerId ? cartState : undefined;
+  console.log("cartData", cartData);
 
   function findCartDetailId(productId: number) {
-    return cart?.cartDetails.find(
+    return cartData?.cartDetails.find(
       (cartDetail) => cartDetail.productId === productId,
     )?.cartDetailId;
   }
 
-  const { updateCartDetail } = UpdateCartDetail();
-  const { deleteCartDetail } = DeleteCartDetail();
+  const productsIdFormRedux = cartState.cartDetails.map(
+    (product) => product.productId,
+  );
 
-  const { cartState, cartDispatch } = useCartData();
-  const navigate = useNavigate();
-
-  const productsId = cartState.cartDetails.map((product) => product.productId);
-  // console.log("productsId", productsId);
   const { data } = useQuery({
-    queryKey: ["productsInCart", productsId],
+    queryKey: ["productsInCart", productsIdFormRedux],
     queryFn: async () => {
-      if (!productsId || productsId.length === 0) return [];
+      if (!productsIdFormRedux || productsIdFormRedux.length === 0) return [];
       return await Promise.all(
-        productsId.map((id) => productService.getProduct(id)),
+        productsIdFormRedux.map((id) => productService.getProduct(id)),
       );
     },
-    enabled: productsId?.length > 0,
+    enabled: productsIdFormRedux?.length > 0,
   });
   const products = data ? data.flatMap((result) => result.payload || []) : [];
 
